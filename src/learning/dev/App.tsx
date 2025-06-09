83631b0bb1e32dec2518ca8f0a6bae35238c65ca
usaganikki <tiro105@gmail.com>
2025-06-09 17:31:09 +0900

    Step1.2の内容コピー、Learning下に専用環境作成

---
+++ src/learning/dev/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { InteractiveCube } from '../step1-testing/InteractiveCube'
+
+const App: React.FC = () => {
+  return (
+    <div style={{
+      fontFamily: 'Arial, sans-serif',
+      height: '100vh', // 追加
+      boxSizing: 'border-box' // 追加 (Reactのstyleではキャメルケース)
+    }}>
+      <InteractiveCube />
+    </div>
+  );
+};
+
+export default App;
